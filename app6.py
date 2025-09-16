bf266ba189468374f950103f132df89b35c5407c
Mary Li <maryli@marys-air.home>
2025-09-16 00:28:55 -0400

    Fix env variable loading and add .gitignore

--- app6.py
+++ app6.py
@@ -17,9 +17,9 @@ print("API KEY LOADED:", os.getenv("AZURE_OPENAI_API_KEY"))
 from openai import AzureOpenAI
 
 client = AzureOpenAI(
-    api_version=os.getenv("AZURE_API_VERSION"),
-    azure_endpoint=os.getenv("AZURE_ENDPOINT"),
-    api_key=os.getenv("AZURE_API_KEY")
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION")
 )
 
 # -----------------------------
